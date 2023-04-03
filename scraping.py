402ee378dcf0122a0dfc079e7163ad3a1e84bdaf
Boutahar Mohammed <mohammed.boutahar@mines-ales.org>
2023-04-03 14:21:58 +0200

    final project

--- scraping.py
+++ scraping.py
@@ -62,17 +62,17 @@ def last_page(url):
 
 
 
-# url1 = "https://www.tripadvisor.com/Hotel_Review-g186338-d282814-Reviews-Abercorn_House-London_England.html"
-# url2 = "https://www.tripadvisor.com/Hotel_Review-g60763-d1181739-Reviews-The_Jane_Hotel-New_York_City_New_York.html"
-# url3 = "https://www.tripadvisor.com/Hotel_Review-g60763-d122005-Reviews-The_New_Yorker_A_Wyndham_Hotel-New_York_City_New_York.html"
-# url4 = "https://www.tripadvisor.com/Hotel_Review-g60763-d93627-Reviews-West_Side_YMCA-New_York_City_New_York.html"
-# url5 = "https://www.tripadvisor.com/Hotel_Review-g60763-d7182733-Reviews-The_Paul_Hotel_NYC-New_York_City_New_York.html"
-# url6 = "https://www.tripadvisor.com/Hotel_Review-g60763-d671150-Reviews-The_Empire_Hotel-New_York_City_New_York.html"
-# url7 = "https://www.tripadvisor.com/Hotel_Review-g60763-d142114-Reviews-Night_Hotel_Broadway-New_York_City_New_York.html"
-# url8 = "https://www.tripadvisor.com/Hotel_Review-g293734-d1873436-Reviews-Riad_Kheirredine-Marrakech_Marrakech_Safi.html"
-# url9 = "https://www.tripadvisor.com/Hotel_Review-g32655-d77237-Reviews-Hollywood_Hotel-Los_Angeles_California.html"
-# url10 = "https://www.tripadvisor.com/Hotel_Review-g32655-d223049-Reviews-Hyatt_Regency_Los_Angeles_International_Airport-Los_Angeles_California.html"
-# url11 = "https://www.tripadvisor.com/Hotel_Review-g32655-d78694-Reviews-Hilton_Los_Angeles_Universal_City-Los_Angeles_California.html"
+url1 = "https://www.tripadvisor.com/Hotel_Review-g186338-d282814-Reviews-Abercorn_House-London_England.html"
+url2 = "https://www.tripadvisor.com/Hotel_Review-g60763-d1181739-Reviews-The_Jane_Hotel-New_York_City_New_York.html"
+url3 = "https://www.tripadvisor.com/Hotel_Review-g60763-d122005-Reviews-The_New_Yorker_A_Wyndham_Hotel-New_York_City_New_York.html"
+url4 = "https://www.tripadvisor.com/Hotel_Review-g60763-d93627-Reviews-West_Side_YMCA-New_York_City_New_York.html"
+url5 = "https://www.tripadvisor.com/Hotel_Review-g60763-d7182733-Reviews-The_Paul_Hotel_NYC-New_York_City_New_York.html"
+url6 = "https://www.tripadvisor.com/Hotel_Review-g60763-d671150-Reviews-The_Empire_Hotel-New_York_City_New_York.html"
+url7 = "https://www.tripadvisor.com/Hotel_Review-g60763-d142114-Reviews-Night_Hotel_Broadway-New_York_City_New_York.html"
+url8 = "https://www.tripadvisor.com/Hotel_Review-g293734-d1873436-Reviews-Riad_Kheirredine-Marrakech_Marrakech_Safi.html"
+url9 = "https://www.tripadvisor.com/Hotel_Review-g32655-d77237-Reviews-Hollywood_Hotel-Los_Angeles_California.html"
+url10 = "https://www.tripadvisor.com/Hotel_Review-g32655-d223049-Reviews-Hyatt_Regency_Los_Angeles_International_Airport-Los_Angeles_California.html"
+url11 = "https://www.tripadvisor.com/Hotel_Review-g32655-d78694-Reviews-Hilton_Los_Angeles_Universal_City-Los_Angeles_California.html"
 
 #Bad reviews
 url12 = "https://www.tripadvisor.com/Hotel_Review-g60763-d99443-Reviews-Park_West_Hotel-New_York_City_New_York.html"
